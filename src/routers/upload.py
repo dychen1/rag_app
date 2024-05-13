5d896d5c8a7d23fab637206830d53495afb36bd0
Denis <dychen93@hotmail.com>
2024-05-13 13:15:40 -0400

    feat: added async retry

--- src/routers/upload.py
+++ src/routers/upload.py
@@ -3,16 +3,20 @@ from minio.error import S3Error
 from minio import Minio
 from minio.versioningconfig import VersioningConfig
 from minio.commonconfig import ENABLED
-from src.utils.minio import get_minio_client
+from typing import BinaryIO
+
 from src.models.response import UploadResponse
+from src.utils.minio import get_minio_client
+from src.utils.decorators import async_retry
 
 
-SUPPORT_FILE_TYPES: list[str] = ["pdf", "tiff", "png", "jpeg"]
+SUPPORT_FILE_TYPES: list[str] = ["pdf", "tiff", "png", "jpeg", "json"]  # Added json to be able to upload samples
+RETRY_ATTEMPTS: int = 3  # Try to upload file 3 times if upload to Minio fails
 router = APIRouter()
 
 
 @router.post("/upload")
-def upload_files(
+async def upload_files(
     files: list[UploadFile],
     client: str = Form(...),
     project: str = Form(...),
@@ -22,6 +26,7 @@ def upload_files(
     empty_files: list[str] = []
     unsupported_files: list[str] = []
     files_too_large: list[str] = []
+    failed_upload_files: list[str] = []
     successfully_uploaded_files: list[str] = []
 
     for file in files:
@@ -38,48 +43,55 @@ def upload_files(
             unsupported_files.append(file.filename)
             continue
 
-        if file.size > 1000 * 1024 * 1024:  # Set file size limit 1GB, needs a front end check if user facing
+        if file.size > 1000 * 1024 * 1024:
             files_too_large.append(file.filename)
             continue
 
-        try:  # Upload file to minio
-            bucket_name = f"{client}-{project}"
-            ensure_bucket_exists(bucket_name, minio_client)
-
-            # Create a new object and stream data to it
-            minio_client.put_object(
-                bucket_name=bucket_name,
-                object_name=file.filename,
-                data=file.file,
-                length=-1,
-                part_size=1024 * 1024 * 10,  # Stream 10MB chunks
-            )
-
-            # Generate a presigned URL for accessing the uploaded file
-            file_url = minio_client.presigned_get_object(bucket_name, file.filename)
+        bucket_name = f"{client}-{project}"
+        await ensure_bucket_exists(minio_client, bucket_name)
+        try:
+            file_name: str = file.filename
+            file_data: BinaryIO = file.file
+            file_url: str = await upload_to_minio(minio_client, bucket_name, file_data, file_name)
             signed_urls.append(file_url)
             successfully_uploaded_files.append(file.filename)
-        except S3Error as e:
-            raise HTTPException(status_code=500, detail=f"Failed to upload {file.filename}: {str(e)}")
+        except HTTPException as e:
+            failed_upload_files.append(file.filename)
 
     # Logging
     details: str = ""
     if empty_files:
-        details += f"Empty file(s): {empty_files}. "
+        details += f"{len(empty_files)} empty file(s): {empty_files}. "
     if unsupported_files:
-        details += f"File(s) with unsupported extension: {unsupported_files}. "
+        details += f"{len(unsupported_files)} file(s) with unsupported extension: {unsupported_files}. "
     if files_too_large:
-        details += f"File(s) which were too large: {files_too_large}. "
+        details += f"{len(files_too_large)} file(s) which were too large: {files_too_large}. "
+    if failed_upload_files:
+        details += f"{len(failed_upload_files)} file(s) failed during upload: {failed_upload_files}. "
 
     if details == "":
         details += "All files successfully uploaded."
     else:
-        details += f"Successfully uploaded files: {successfully_uploaded_files}."
+        details += f"{len(successfully_uploaded_files)} successfully uploaded files: {successfully_uploaded_files}."
 
     return UploadResponse(signed_urls=signed_urls, details=details)
 
 
-def ensure_bucket_exists(bucket_name: str, minio_client: Minio) -> None:
+@async_retry(max_attempts=3, initial_delay=2, backoff_factor=2)
+async def upload_to_minio(minio_client: Minio, bucket_name: str, file_data: BinaryIO, file_name: str) -> str:
+    # Create a new object and stream data to it
+    minio_client.put_object(
+        bucket_name=bucket_name,
+        object_name=file_name,
+        data=file_data,
+        length=-1,  # -1 is used when the size of file uploads is unknown
+        part_size=1024 * 1024 * 10,  # Upload file in 10MB chunks
+    )
+    # Generate a presigned URL for accessing the uploaded file, using default 7 day expiration
+    return minio_client.presigned_get_object(bucket_name, file_name)
+
+
+async def ensure_bucket_exists(minio_client: Minio, bucket_name: str) -> None:
     try:
         if not minio_client.bucket_exists(bucket_name):
             minio_client.make_bucket(bucket_name)
---
+++ src/utils/decorators.py
@@ -0,0 +1,37 @@
+import asyncio
+from functools import wraps
+from fastapi import HTTPException
+
+
+def async_retry(max_attempts: int = 3, initial_delay: int = 1, backoff_factor: int = 2):
+    """
+    Decorator to retry an asynchronous task with exponential backoff.
+
+    Args:
+        max_attempts (int): Maximum number of retry attempts.
+        initial_delay (int): Initial delay between retries in seconds.
+        backoff_factor (int): Factor by which to multiply the delay for each subsequent retry.
+
+    Returns:
+        Decorated function with retry capability.
+    """
+
+    def decorator(func):
+        @wraps(func)
+        async def wrapper(*args, **kwargs):
+            delay = initial_delay
+            attempts = 0
+            while attempts < max_attempts:
+                try:
+                    return await func(*args, **kwargs)
+                except Exception as e:
+                    attempts += 1
+                    if attempts >= max_attempts:
+                        raise HTTPException(status_code=500, detail=str(e)) from e
+                    print(f"{func.__name__}: Attempt {attempts} failed with error {e}. Retrying in {delay} seconds...")
+                    await asyncio.sleep(delay)
+                    delay *= backoff_factor
+
+        return wrapper
+
+    return decorator
