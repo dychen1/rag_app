13026a88afe3f67093d22c290803984067e9da27
Denis <dychen93@hotmail.com>
2024-05-17 16:34:40 -0400

    fix: sample files dir name
    chore: delete etc/samples_files directory

--- src/app.py
+++ src/app.py
@@ -5,6 +5,7 @@ from src.utils.clients import get_minio_client
 from src.utils.middleware.auth import AuthMiddleware
 from src import ENV
 
+
 app = FastAPI()
 app.add_middleware(AuthMiddleware, api_key=ENV["API_KEY"])
 app.include_router(upload.router, dependencies=[Depends(get_minio_client)])
