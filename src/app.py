977b8bd1fe6dacc3ddd1ca070450d1f1d6d2bf99
Denis <dychen93@hotmail.com>
2024-05-17 16:23:25 -0400

    feat: added auth middleware

--- src/app.py
+++ src/app.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI, Depends
 
 from src.routers import upload, create_embeddings, query
 from src.utils.clients import get_minio_client
-
+from src.utils.middleware.auth import AuthMiddleware
+from src import ENV
 
 app = FastAPI()
+app.add_middleware(AuthMiddleware, api_key=ENV["API_KEY"])
 app.include_router(upload.router, dependencies=[Depends(get_minio_client)])
 app.include_router(create_embeddings.router)
 app.include_router(query.router)
---
+++ src/utils/middleware/auth.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI, Request
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.responses import JSONResponse
+from typing import Callable
+
+
+class AuthMiddleware(BaseHTTPMiddleware):
+    def __init__(self, app: FastAPI, api_key: str):
+        """
+        Middleware to enforce API key authentication on incoming requests.
+
+        Args:
+            app (FastAPI): FastAPI application instance.
+            api_key (str): API key used for authenticating requests.
+        """
+        super().__init__(app)
+        self.api_key = api_key
+
+    async def dispatch(self, request: Request, call_next: Callable):
+        """
+        Process the incoming request and enforce API key authentication.
+
+        Args:
+            request (Request): The incoming request object.
+            call_next (Callable): The next middleware or route handler.
+
+        Returns:
+            Response: The response object.
+
+        Raises:
+            HTTPException: If the API key is invalid or missing.
+        """
+        api_key = request.headers.get("x-api-key")
+        if api_key != self.api_key:
+            return JSONResponse(
+                status_code=403,
+                content={"detail": "Forbidden: Invalid API Key"},
+            )
+        response = await call_next(request)
+        return response
