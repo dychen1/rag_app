c6c7dc52a7b5577af57026ccaaeb354470a2f5a0
Denis <dychen93@hotmail.com>
2024-05-20 17:32:46 -0400

    build: added gha workflow for building and pushing docker images

--- src/app.py
+++ src/app.py
@@ -5,10 +5,9 @@ from src.routers import create_embeddings, query, upload
 from src.utils.clients import get_minio_client
 from src.utils.middleware.auth import AuthMiddleware
 
+
 app = FastAPI()
 app.add_middleware(AuthMiddleware, api_key=ENV["API_KEY"])
 app.include_router(upload.router, dependencies=[Depends(get_minio_client)])
 app.include_router(create_embeddings.router)
 app.include_router(query.router)
-
-print("test")
